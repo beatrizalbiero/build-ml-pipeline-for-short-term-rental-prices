afe8e96ea38940307e4abe9ef6bee9e61d1bdd7a
Beatriz Albiero <beatriz.albiero@willbank.com.br>
2023-02-08 16:53:25 -0300

    updated geo boundaries in basic_cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,9 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
     logger.info("Converted last_review to datetime.")
     logger.info("{}".format(df.info()))
+    # Set geo boundaries
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     #Upload to W&B
     df.to_csv("clean_sample.csv", index=False)
